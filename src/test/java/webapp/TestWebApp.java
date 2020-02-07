3e86badd9a87fd0a9cf3c1ffcd5de4a8551c9416
michael <michaelwdysart@gmail.com>
2020-02-07 15:17:04 -0500

    The tests were failing. So I just removed the tests that failed and now it works :D

--- src/test/java/webapp/TestWebApp.java
+++ src/test/java/webapp/TestWebApp.java
@@ -1,5 +1,6 @@
 package webapp;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -21,35 +23,25 @@ public class TestWebApp {
     private MockMvc mockMvc;
 
     @Test
-    public void shouldCreateBook() throws Exception {
-        this.mockMvc.perform(get("/create?bookName=ABC")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Create Success")));
-    }
-
-    @Test
-    public void shouldAddBuddies() throws Exception {
+    public void shouldCheckSequence() throws Exception {
         this.mockMvc.perform(get("/create?bookName=ABC")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Create Success")));
 
         this.mockMvc.perform(post("/add?bookName=ABC&name=Joe&address=Tatooine&phoneNumber=555-5555")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Add Buddy Success")));
+                .andExpect(content().string(containsString("Add Buddy Success")))
+                .andExpect(content().string(containsString("Joe : Tatooine : 555-5555;")));
 
         this.mockMvc.perform(post("/add?bookName=ABC&name=Bob&address=Manaan&phoneNumber=555-5555")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Add Buddy Success")));
-    }
+                .andExpect(content().string(containsString("Add Buddy Success")))
+                .andExpect(content().string(containsString("Bob : Manaan : 555-5555;")));
 
-    @Test
-    public void shouldRemoveBuddy() throws Exception {
-        this.mockMvc.perform(get("/create?bookName=ABC")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Create Success")));
+        this.mockMvc.perform(post("/remove?bookName=ABC&name=Joe")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Remove Buddy Success")))
+                .andExpect(content().string(containsString("Bob : Manaan : 555-5555;")))
+                .andExpect(content().string(not(containsString("Joe : Tatooine : 555-5555;"))));
 
-        this.mockMvc.perform(post("/add?bookName=ABC&name=Joe&address=Tatooine&phoneNumber=555-5555")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Add Buddy Success")));
-
-        this.mockMvc.perform(post("/add?bookName=ABC&name=Bob&address=Manaan&phoneNumber=555-5555")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Add Buddy Success")));
-
-        this.mockMvc.perform(post("/remove?bookName=ABC&name=Bob")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Remove Buddy Success")));
+        this.mockMvc.perform(get("/view?bookName=ABC")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Bob : Manaan : 555-5555;")))
+                .andExpect(content().string(not(containsString("Joe : Tatooine : 555-5555;"))));
     }
 }
